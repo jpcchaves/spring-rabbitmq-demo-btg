f3d60bf87a1719cdf710bb1d8a1c075d8fa091bf
jpcchaves <jpcchaves@outlook.com>
2024-10-15 15:03:45 -0300

    first commit

---
+++ src/test/java/com/tutorial/sr/springrabbit/SpringrabbitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tutorial.sr.springrabbit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringrabbitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
